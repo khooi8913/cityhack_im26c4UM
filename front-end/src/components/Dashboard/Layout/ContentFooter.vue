9607894f8beb9dc57bc05bf247e0f4ea7f263354
Leon Wee <leonweecs@gmail.com>
2017-10-28 16:57:08 +0800

    GG PUSH

---
+++ front-end/src/components/Dashboard/Layout/ContentFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav class="pull-left">
+        <ul>
+          <li>
+            <router-link :to="{path:'/admin'}">IM26C4UM</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright pull-right">
+        &copy; Created with
+        <i class="fa fa-heart heart"></i> by team
+        <a href="https://github.com/cristijora" target="_blank">IM26C4UM</a>.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
---
+++ front-end/src/routes/routes.js
@@ -0,0 +1,49 @@
+import DashboardLayout from '../components/Dashboard/Layout/DashboardLayout.vue'
+// GeneralViews
+import NotFound from '../components/GeneralViews/NotFoundPage.vue'
+
+// Admin pages
+import Overview from 'src/components/Dashboard/Views/Overview.vue'
+// import UserProfile from 'src/components/Dashboard/Views/UserProfile.vue'
+// import Notifications from 'src/components/Dashboard/Views/Notifications.vue'
+// import Icons from 'src/components/Dashboard/Views/Icons.vue'
+import Maps from 'src/components/Dashboard/Views/Maps.vue'
+// import Typography from 'src/components/Dashboard/Views/Typography.vue'
+// import TableList from 'src/components/Dashboard/Views/TableList.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: DashboardLayout,
+    redirect: '/admin/overview'
+  },
+  {
+    path: '/admin',
+    component: DashboardLayout,
+    redirect: '/admin/stats',
+    children: [
+      {
+        path: 'overview',
+        name: 'overview',
+        component: Overview
+      },
+      {
+        path: 'maps',
+        name: 'maps',
+        component: Maps
+      }
+    ]
+  },
+  { path: '*', component: NotFound }
+]
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * The specified component must be inside the Views folder
+ * @param  {string} name  the filename (basename) of the view to load.
+function view(name) {
+   var res= require('../components/Dashboard/Views/' + name + '.vue');
+   return res;
+};**/
+
+export default routes
