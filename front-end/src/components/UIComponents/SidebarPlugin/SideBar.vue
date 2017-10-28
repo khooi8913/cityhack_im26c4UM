8084aff381dec992d7f00f059177b4a4cbfcbd2e
Leon Wee <leonweecs@gmail.com>
2017-10-28 18:43:47 +0800

    Added vehicles number micro trend

--- front-end/src/components/UIComponents/SidebarPlugin/SideBar.vue
+++ front-end/src/components/UIComponents/SidebarPlugin/SideBar.vue
@@ -11,7 +11,7 @@
             <div class="logo-img">
                 <img src="static/img/sunway_logo.png" alt="">
             </div>
-          Sunway City
+          Sunway City Hack
         </a>
       </div>
       <slot>
