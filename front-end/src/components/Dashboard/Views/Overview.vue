c8b232417fb73f621ae5c27f86510e784cf88df4
kamwoh <kamwoh@neruti.com>
2017-10-29 00:36:17 +0800

    fk this modal

--- front-end/src/components/Dashboard/Views/Overview.vue
+++ front-end/src/components/Dashboard/Views/Overview.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!--Stats cards-->
     <div class="row">
       <div class="col-lg-3 col-sm-6" v-for="stats in statsCards">
         <stats-card>
@@ -20,17 +19,17 @@
 
     <div class="row">
 
-        <div class="col-lg-6">
-          <chart-card :chart-data="usersChart.data" :chart-options="usersChart.options">
-            <h4 class="title" slot="title">Vehicles on road (micro-trend)</h4>
-            <span slot="subTitle"> 24 Hours performance</span>
-            <span slot="footer">
+      <div class="col-lg-6">
+        <chart-card :chart-data="usersChart.data" :chart-options="usersChart.options">
+          <h4 class="title" slot="title">Vehicles on road (micro-trend)</h4>
+          <span slot="subTitle"> 24 Hours performance</span>
+          <span slot="footer">
               <i class="ti-reload"></i> Updated 3 minutes ago</span>
-            <div slot="legend">
-              <i class="fa fa-circle text-info"></i> Vehicle
-            </div>
-          </chart-card>
-        </div>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Vehicle
+          </div>
+        </chart-card>
+      </div>
 
       <div class="col-lg-6">
         <div class="card">
@@ -43,33 +42,33 @@
       <!-- <div class="row"> -->
 
 
-<!-- 
-        <div class="col-md-6 col-xs-12">
-          <chart-card :chart-data="preferencesChart.data"  chart-type="Pie">
-            <h4 class="title" slot="title">Email Statistics</h4>
-            <span slot="subTitle"> Last campaign performance</span>
-            <span slot="footer">
-              <i class="ti-timer"></i> Campaign set 2 days ago</span>
-            <div slot="legend">
-              <i class="fa fa-circle text-info"></i> Open
-              <i class="fa fa-circle text-danger"></i> Bounce
-              <i class="fa fa-circle text-warning"></i> Unsubscribe
-            </div>
-          </chart-card>
-        </div>
+      <!--
+              <div class="col-md-6 col-xs-12">
+                <chart-card :chart-data="preferencesChart.data"  chart-type="Pie">
+                  <h4 class="title" slot="title">Email Statistics</h4>
+                  <span slot="subTitle"> Last campaign performance</span>
+                  <span slot="footer">
+                    <i class="ti-timer"></i> Campaign set 2 days ago</span>
+                  <div slot="legend">
+                    <i class="fa fa-circle text-info"></i> Open
+                    <i class="fa fa-circle text-danger"></i> Bounce
+                    <i class="fa fa-circle text-warning"></i> Unsubscribe
+                  </div>
+                </chart-card>
+              </div>
 
-        <div class="col-md-6 col-xs-12">
-          <chart-card :chart-data="activityChart.data" :chart-options="activityChart.options">
-            <h4 class="title" slot="title">2015 Sales</h4>
-            <span slot="subTitle"> All products including Taxes</span>
-            <span slot="footer">
-              <i class="ti-check"></i> Data information certified</span>
-            <div slot="legend">
-              <i class="fa fa-circle text-info"></i> Tesla Model S
-              <i class="fa fa-circle text-warning"></i> BMW 5 Series
-            </div>
-          </chart-card>
-        </div> -->
+              <div class="col-md-6 col-xs-12">
+                <chart-card :chart-data="activityChart.data" :chart-options="activityChart.options">
+                  <h4 class="title" slot="title">2015 Sales</h4>
+                  <span slot="subTitle"> All products including Taxes</span>
+                  <span slot="footer">
+                    <i class="ti-check"></i> Data information certified</span>
+                  <div slot="legend">
+                    <i class="fa fa-circle text-info"></i> Tesla Model S
+                    <i class="fa fa-circle text-warning"></i> BMW 5 Series
+                  </div>
+                </chart-card>
+              </div> -->
 
     </div>
   </div>
@@ -86,21 +85,21 @@
       id: 1,
       name: 'Road accident',
       location: 'Jalan PJS 11/20',
-      time: '9:32 AM, 29/10/17',
+      time: '9:32:12 AM, 29/10/2017',
       gps: '3.068331, 101.602162'
     },
     {
       id: 2,
       name: 'Robbery',
       location: 'Jalan Universiti',
-      time: '9:57 AM, 29/10/17',
+      time: '9:57:54 AM, 29/10/2017',
       gps: '3.067661, 101.603634'
     },
     {
       id: 3,
       name: 'Street Demonstrations',
       location: 'Persiaran Lagoon',
-      time: '10:13 AM, 29/10/17',
+      time: '10:13:23 AM, 29/10/2017',
       gps: '3.072813, 101.608980'
     }]
   export default {
@@ -121,7 +120,7 @@
             icon: 'ti-car',
             title: 'Vehicles on road',
             value: '698',
-            footerText: 'Updated 5 mins ago',
+            footerText: 'Updated 2 seconds ago',
             footerIcon: 'ti-reload'
           },
           {
@@ -213,6 +212,29 @@
             type: receivedMessage.type
           }
         )
+      },
+      calcRoute (map, directionsService, directionsDisplay) {
+        let start = new window.google.maps.LatLng(3.0714, 101.6052)
+
+        let end = new window.google.maps.LatLng(3.0662, 101.6096)
+
+        let bounds = new window.google.maps.LatLngBounds()
+        bounds.extend(start)
+        bounds.extend(end)
+        map.fitBounds(bounds)
+        let request = {
+          origin: start,
+          destination: end,
+          travelMode: window.google.maps.TravelMode.DRIVING
+        }
+        directionsService.route(request, (response, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response)
+            directionsDisplay.setMap(map)
+          } else {
+            alert('Directions Request from ' + start.toUrlValue(6) + ' to ' + end.toUrlValue(6) + ' failed: ' + status)
+          }
+        })
       }
     },
     created () {
@@ -226,9 +248,6 @@
 
       ws.onmessage = function (evt) {
         const receivedMsg = JSON.parse(evt.data)
-        // {dialog_message: ''}
-
-        const message = '<div>fsdfsdfsdf</div>'
 
         const options = {
           html: true, // set to true if your message contains HTML tags. eg: "Delete <b>Foo</b> ?"
@@ -242,7 +261,7 @@
           clicksCount: 3 // for soft confirm, user will be asked to click on "proceed" btn 3 times before actually proceeding
         }
 
-        dialog.confirm(message, options)
+        dialog.confirm('<img src="../../static/img/test_gif.gif" class="map-container">', options)
           .then(
             (d) => {
               setTimeout(
@@ -262,6 +281,15 @@
             })
           })
 
+        _this.statsCards[2].value = parseInt(_this.statsCards[2].value) + 1
+        const datetime = new Date()
+        _this.table.data = [..._this.table.data, {
+          id: 4,
+          name: receivedMsg.problem,
+          location: 'Canopy Walk',
+          gps: '3.059221, 101.607812',
+          time: `${datetime.toLocaleTimeString()}, ${datetime.toLocaleDateString()}`
+        }]
         console.log(receivedMsg)
       }
 
@@ -277,10 +305,17 @@
       window.onbeforeunload = function (event) {
         ws.close()
       }
+
+      setInterval(() => {
+        _this.statsCards[0].value = 698 + parseInt(Math.random() * 40 - 20)
+      }, 2000)
     }
   }
 
 </script>
 <style>
-
+  .map-container {
+    width: 100%;
+    height: 100%;
+  }
 </style>
